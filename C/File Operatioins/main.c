3ac64ba78be2e4f37d2a05e41d80ec37156b0d1a
Skullfire <onlyforworkplsremember@gmail.com>
2026-03-17 01:02:29 +0530

    Started learning C again

---
+++ C/File Operatioins/main.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+
+
+/*
+
+Terminology:
+
+(i) file stream : consider this well like a river, if you get me. But in seriousness
+                  all a stream represents if something you write into, or read from
+                  it can stdin for readingf, stdout for writing, or a FILE pointer
+
+(ii) EOF (end of file) : kind of self explanatory, but it just means the ending of a file
+
+(iii) Current Working Directory or CWD : it is the directory where your program currently looks for files for reading, or writing
+
+*/
+
+
+
+int main(){
+
+    // fopen is the function which returns a pointer to
+    // a file, it takes two arguments, the name of the file
+    // and the mode you want to open it in
+
+    // There are four modes you can open a file in
+    // (i) reading ("r")
+    // (ii) writing ("w")
+    // (iii) writing in binary ("wb")
+    // (iv) reading in binary ("rb")
+
+    FILE* f = fopen("dummy.txt", "w");
+
+    // now if you have opened the file in writing mode
+    // you can write single characters as follows
+    
+    fputc('H', f);
+    fputc('e', f);
+    fputc('l', f);
+    fputc('l', f);
+    fputc('o', f);
+    fputc('\n', f);
+
+    // or you can write a string as follows
+    fprintf(f, "world!");
+
+    // after doing whatever it is you wanted to do with the file
+    // you have to close the file as follows
+
+    fclose(f);
+
+    f = fopen("dummy.txt", "r");
+
+    // now if you want to read a file
+    // then here is how you do it
+
+    // if you want to read single characters
+    // you do it like so
+
+    char temp = fgetc(f);
+
+    putc(temp, stdout); // here stdout refers to the console
+
+    // each successive call to fgetc will fetch the next character from
+    // the file stream
+
+    //now if you want to read a string, you do it like so ma boi
+
+    char buf[256];
+    fgets(buf, sizeof(buf), f);
+
+    printf("%s\n", buf);
+
+    // but you see this has a problem, it stop reading when it hits a \n character
+    // so the better solution would be
+
+    fread(buf, sizeof(buf), 10, f);
+
+    // fread reads until EOF or the number of objects read exceeds the number of objects to be read
+    // provided as the 3rd argument
+
+    printf("%s\n", buf);
+
+    fclose(f);
+
+    // you can also rename a file like so
+
+    int renamed = rename("dummy.txt", "dum.txt");
+
+    if(renamed){
+        perror("Failed to rename file !");
+    }else{
+        printf("Renamed file to dum.txt\n");
+    }
+
+    // you can also remove files from the current working directory, like so
+
+    int removed = remove("dum.txt");
+
+    if(removed){
+        perror("Failed to remove file!");
+    }else{
+        printf("Successfully remove file.\n");
+    }
+    
+    return 0;
+}
---
+++ C/Printing Stuff/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    // printf is the standard way to print something on the console
+    // it is defined in the stdio header
+    printf("Hello, world!\n");
+
+    // you can use subsitution for printing stuff, like so
+
+    printf("I am %04d years old.\n", 19);
+
+    // or you can use a variable like so
+
+    const char* name = "Skullfire-GT04";
+
+    printf("My github name is %s.\n", name);
+
+    // you can also use floating point numbers as subsitution during printing
+
+    const float num1 = 2.142354f; // the 'f' is to indicate that this is a float not a double to the compiler
+
+    printf("Here is a random number %04.2f\n", num1);
+
+    // now say you want to print something which indicates an error has occured
+    // you do so like so
+
+    perror("Something went wrong !");
+    // perror should be read as "print error"
+
+    // now Idk why would you wanna print a single character on the console
+    // but here is how you do it
+
+    putc('D', stdout);
+    putc('a', stdout);
+    putc('m', stdout);
+    putc('\n', stdout);
+
+    return EXIT_SUCCESS;
+}
