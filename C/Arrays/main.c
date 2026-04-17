bfe755d8065e2c93b8baa4dc3d420715bae6e782
Skullfire-GT04 <betterthanthelastonel0l@gmail.com>
2026-04-18 01:46:14 +0530

    Learned more about C arrays

--- C/Arrays/main.c
+++ C/Arrays/main.c
@@ -26,7 +26,51 @@ int main(){
     they are less than or equal to the size passed into the 'size' argument during array declaration. Whereas in the third method, the 
     number of elements you intialize becomes the total size of the array.
 
+    In C array names are just pointers (see notes), they point to the first element of the array
+
     */
+
+    // Here is how you access elements in arrays
+    for(int i = 0; i < 10; i++){
+        printf("integers[%d]: %d\n", i, integers[i]);
+    }
+
+    // you can use the elements of an array as regular variables
+    int a = (integers[7] - integers[2]) * integers[3];
+    printf("a: %d\n", a);
+
+    // you can check that the name of an array is just a pointer like so
+    printf("integers: %d, &integers[0]: %d\n", integers, &integers[0]);
+
+    // here is how you can make an array in the heap (see notes)
+    int* pInts = (int *) malloc(sizeof(int) * 10);
+
+    // you can use it like so
+    pInts[0] = 10;
+
+    // here pInts is a pointer to the 0th element of the array
+    // you can access elements using a dereference syntax too
+    *pInts; pInts[0]; // are both the same
+
+    // or something like this
+    *(pInts + 2); pInts[2]; // both are the same
+
+    *(pInts + 4) = 12;
+
+    // you can check it like so
+    printf("pInts[4]: %d, *(pInts + 4): %d\n", pInts[4], *(pInts + 4));
+
+    // when you use calloc instead of malloc, it initalizes all the elements to zero
+    float* pFloats = (float*) calloc(sizeof(float), 10);
+
+    // here let's check
+    printf("Initialization using calloc:\n");
+    for(int i = 0; i < 10; i++){
+        printf("pFloats[%d]: %f\n", i, pFloats[i]);
+    }
+
+    free(pInts);
+    free(pFloats);
 
     return 0;
 }
