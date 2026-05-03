3a06d2bc940c4d2a373c91942e73d2d985067d04
Skullfire-GT04 <betterthanthelastonel0l@gmail.com>
2026-05-04 00:00:23 +0530

    Learned About Dynamic Arrays in Java

--- Java/ArrayDemo/Main.java
+++ Java/ArrayDemo/Main.java
@@ -1,5 +1,7 @@
 package Java.ArrayDemo;
 
+import java.util.ArrayList;
+
 public class Main{
     
     public static void main(String[] args){
@@ -21,5 +23,29 @@ public class Main{
             System.out.println("intArr[" + i + "] : " + intArr[i]);
         }
 
+        // aside from normal static arrays, java also has a built-in dynamic array
+        // class called the ArrayList
+
+        // the format for instantiating one is like so
+        // ArrayList identifier = new ArrayList<DataType>();
+
+        ArrayList grocery_list = new ArrayList<String>();
+
+        // adding items to the array list
+        grocery_list.add("Lettuce");
+        grocery_list.add("Onions");
+        grocery_list.add("Spinach");
+        grocery_list.add("Cheese");
+
+        // deleting items
+        grocery_list.remove("Spinach")
+
+        // accessing elements
+        String food = grocery_list.get(0);
+
+        // getting the length of the grocery list
+        int size = grocery_list.size();
+
+        
     }
 }
