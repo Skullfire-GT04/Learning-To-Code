36c489b944f61770f0244d4cdd42ea984e1b5a0b
Skullfire-GT04 <betterthanthelastonel0l@gmail.com>
2026-05-13 21:16:36 +0530

    Learned about loop in javascript

---
+++ WebTech/Basic Javascirpt/loops.js
@@ -0,0 +1,71 @@
+// Let's look at loops in javascript
+
+
+// Let's start with the while loop
+/* 
+The format is like so:
+while(test-condition){
+    .
+    .
+    .
+}
+*/
+
+let i = 0;
+while(i < 10){
+    console.log(`i : ${i}`);
+    i++;
+}
+
+// Let's look at do-while loops now
+/*
+The format is like so:
+do{
+    .
+    .
+    .
+}while(test-condition);
+*/
+
+let num = 5;
+i = 1;
+do{
+    console.log(`5 x ${i} : ${num}`);
+    num += 5;
+    i++;
+}while(num < 50);
+
+
+// Let's look at the for loop now
+/*
+The format is like so:
+
+for(variable_declarations; test-condition; increment/decrement operations){
+    .
+    .
+    .
+}
+
+*/
+// Like
+
+let foods = ["Apple", "Banana", "Orange", "Pineapple"];
+for(let index = 0; index < foods.length; index++){
+    console.log(`Foods[${index}] : ${foods[index]}`);
+}
+
+// Let look at a for-in loop now
+/*
+The general format is:
+
+for(let variable_name of iterable){
+    .
+    .
+    .
+}
+*/
+
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for(let num of nums){
+    console.log(`${num} x ${num} : ${num ** 2}`);
+}
